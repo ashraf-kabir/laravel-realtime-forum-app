f10a4ae35ffbfc0dc3caeb134a1e48925579f2d9
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-10 21:49:28 +0600

    question table updated

--- database/migrations/2020_04_10_153414_create_questions_table.php
+++ database/migrations/2020_04_10_153414_create_questions_table.php
@@ -15,6 +15,12 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
         });
     }
