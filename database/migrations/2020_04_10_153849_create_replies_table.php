06f844bcae11cadffdf8b98fe85bf0dcbef1e5df
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-10 22:01:09 +0600

    replies table updated

--- database/migrations/2020_04_10_153849_create_replies_table.php
+++ database/migrations/2020_04_10_153849_create_replies_table.php
@@ -15,6 +15,12 @@ class CreateRepliesTable extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
+
+            $table->bigInteger('question_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
