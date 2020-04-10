93ba4c81f3af4434b32a7c8c844c25375eb18184
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-10 21:45:13 +0600

    like model created

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Like;
+use Faker\Generator as Faker;
+
+$factory->define(Like::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
