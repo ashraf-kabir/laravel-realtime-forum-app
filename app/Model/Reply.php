ab7692049e4993db91d3add148e7c34d526a7676
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-11 00:49:49 +0600

    reply and user relationship created

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,20 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question() {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function like() {
+        return $this->hasMany(Like::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Model\Question;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +37,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function question() {
+        return $this->hasMany(Question::class);
+    }
 }
