1cc131d6b01f510a7ac57c310515b436ecca16e7
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-11 00:46:32 +0600

    question relationship

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,21 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies() {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
 }
