ac9760cf42a46d6915a24fc8851dbc842e6c8e08
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-12 23:55:12 +0600

    like factory updated

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -3,10 +3,13 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Like;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
