bdbd37c262fdcf9556849f639a6c18e53c117a09
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-12 23:52:43 +0600

    updated

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -2,11 +2,19 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Question;
 use App\Model\Reply;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function() {
+            return Question::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
