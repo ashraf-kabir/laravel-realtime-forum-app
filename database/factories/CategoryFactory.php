97d74e1dedbd30e922b6defc18cf82035c41c8fa
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-12 23:54:13 +0600

    category factory updated

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -6,7 +6,9 @@ use App\Model\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name' => $word,
+        'slug' => str_slug($word)
     ];
 });
