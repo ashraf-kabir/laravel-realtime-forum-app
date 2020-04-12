cc635884af3057d7696a22bfef0116b4d082103d
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-12 23:40:05 +0600

    question factory updated

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Category;
 use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => str_slug($title),
+        'body' => $faker->text,
+        'category_id' => function() {
+            return Category::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
