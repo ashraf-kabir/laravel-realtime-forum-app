7932d3e37bdd86ab8e7fe2a55135ecfd0ab2f815
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-04-13 00:13:15 +0600

    database seeded

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Model\Category;
+use App\Model\Like;
+use App\Model\Question;
+use App\Model\Reply;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class, 10)->create();
+        factory(Category::class, 5)->create();
+        factory(Question::class, 10)->create();
+        factory(Reply::class, 50)->create()->each(function($reply) {
+            return $reply->like()->save(factory(Like::class)->make());
+        });
     }
 }
