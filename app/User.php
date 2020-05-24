910a727e673336056549ef515109484d14d3486e
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-05-25 04:00:52 +0600

    jwt auth added

--- app/User.php
+++ app/User.php
@@ -6,8 +6,9 @@ use App\Model\Question;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -40,5 +41,25 @@ class User extends Authenticatable
 
     public function question() {
         return $this->hasMany(Question::class);
+    }
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
