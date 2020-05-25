144b46cf06568e57612cb4e23619143fe98dfbd5
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-05-26 03:02:28 +0600

    vuejs and vuetify installed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div id="app">
+        <v-app>
+            
+        </v-app>
+    </div>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
