d3922952dfc3997c39cf95b72d82af888ede0fd8
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-05-25 10:09:27 +0600

    middleware and exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,12 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +55,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceOf TokenBlacklistedException) {
+            return response(['error'=>'Token cannot be used, get new one'], Response::HTTP_BAD_REQUEST);
+        } elseif ($exception instanceOf TokenInvalidException) {
+            return response(['error'=>'Token is invalid'], Response::HTTP_BAD_REQUEST);
+        } elseif($exception instanceOf TokenExpiredException) {
+            return response(['error'=>'Token is expired'], Response::HTTP_BAD_REQUEST);
+        } elseif ($exception instanceOf JWTException) {
+            return response(['error'=>'Token is not provided'], Response::HTTP_BAD_REQUEST);
+        }
         return parent::render($request, $exception);
     }
 }
