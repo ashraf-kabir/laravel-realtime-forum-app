4ed4231b3a6639c025c9bd3055ca31c742aead56
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-05-27 11:40:24 +0600

    vue router added

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,7 +24,8 @@ Vue.use(Vuetify)
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('AppHome', require('./components/AppHome.vue').default);
+import router from './Router/router.js'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -34,4 +35,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer class="pa-3">
+    Brand
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
